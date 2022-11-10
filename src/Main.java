6c80999415554d76f9c7ae0be3b32e76cf137e29
Marcus H <78554363+MarcusH3@users.noreply.github.com>
2022-11-10 15:56:16 +0100

    InterestrateCalculator

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 2.0));
+
+        for (int i = 4; i < 5; i++) {
+            System.out.println("Loop " + i + " hello!");
+        }
+        for (int i = 2; i < 9; i++) {
+            System.out.println("10,000 at " + i + " % interest = " + String.format("%.0f", calculateInterest(10000.0, i)));
+        }
+        for (int i = 9; i > 1; i--) {
+            System.out.println("10,000 at " + i + " % interest = " + String.format("%.0f", calculateInterest(10000.0, i)));
+        }
+    }
+
+    public static double calculateInterest(double amount, double interestRate){
+        return (amount * (interestRate/100));
+    }
+}
